9b76d7f3ed3b8a4b66a0fc959a4e11c5e7bb4f16
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-02 08:37:45 +0530

    Validating Mobile Number

--- JUnitUserRegistration.java
+++ JUnitUserRegistration.java
@@ -2,17 +2,21 @@ package jUnitPattern;
 
 import java.util.Scanner;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
+@FixMethodOrder(MethodSorters.JVM)
 public class JUnitUserRegistration {
 	
 	TestClass test = new TestClass();
 	Scanner scan = new Scanner(System.in);
 	
 	String nameRejex = "^[A-Z]{1}+[A-Za-z]{2,}";
+	String mobileNumberRejex = "[+]{0,1}+91+[ ]+[1-9]{1}+[0-9]{9}";
 
 	@Test
-	public void testFirstName() { //Tests First Name
+	public void firstName() { //Tests First Name
 		
 		System.out.print("First Name : ");
 		String firstName = scan.next();
@@ -21,15 +25,26 @@ public class JUnitUserRegistration {
 		System.out.println(firstName+" : "+result);
 		Assert.assertEquals("Validated", result);	
 	}
-	
+
 	@Test
-	public void testLastName() { //Tests Last Name
+	public void lastName() { //Tests Last Name
 		
 		System.out.print("Last Name : ");
 		String lastName = scan.next();
 		
 		String result = test.testPattern(nameRejex, lastName);
 		System.out.println(lastName+" : "+result);
+		Assert.assertEquals("Validated", result);
+	}
+	
+	@Test
+	public void mobileNumber() { //Tests Mobile Number
+		
+		System.out.print("Mobile Number : ");
+		String mobileNumber = scan.nextLine();
+		
+		String result = test.testPattern(mobileNumberRejex, mobileNumber);
+		System.out.println(mobileNumber+" : "+result);
 		Assert.assertEquals("Validated", result);
 	}
 }
