6eac00ae0ef420dd8ea289809bab6a18d019a017
JONNAGADDALA RAVIKUMAR <70770320+JONNAGADDALARAVIKUMAR@users.noreply.github.com>
2020-11-03 18:13:39 +0530

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class TestClass {
 
-	public String testPattern(String nameRejex, String firstName) {
+	public String testPattern(String pattern, String stringTo) {
 		
 		return (Pattern.matches(nameRejex, firstName)?"Validated":"Invalid");	
 	}
