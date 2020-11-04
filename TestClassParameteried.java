48369dc9afd12d101df929f391e5b24973896a09
JONNAGADDALA RAVIKUMAR <70770320+JONNAGADDALARAVIKUMAR@users.noreply.github.com>
2020-11-04 09:04:45 +0530

    Update TestClassParameteried.java

--- TestClassParameteried.java
+++ TestClassParameteried.java
@@ -4,11 +4,11 @@ import java.util.regex.Pattern;
 
 public class TestClassParameteried {
 
-	public static final String emailRejex = "[a-zA-Z]{1,}?[A-Za-z0-9]{0,1}+([-._+]{0,1}+[a-zA-Z0-9]{1,}){0,1}"
+	public static final String emailRejax = "[a-zA-Z]{1,}?[A-Za-z0-9]{0,1}+([-._+]{0,1}+[a-zA-Z0-9]{1,}){0,1}"
 			+ "+@+[a-zA-Z0-9]{1,}?([-._]{0,1}+[a-zA-Z0-9]{2,}){0,1}?([-._]{0,1}"
 			+ "+[a-zA-Z]{2,}){0,1}";
 	
 	public boolean testemailPattern_WithSamples(String emailExample, boolean emailResult) {
-		return (Pattern.matches(emailRejex, emailExample) == emailResult);
+		return (Pattern.matches(emailRejax, emailExample) == emailResult);
 	}
 }
